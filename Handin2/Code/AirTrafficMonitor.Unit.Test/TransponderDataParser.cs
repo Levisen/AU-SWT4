8132848e6af130cc5e21b89ec854e5b766872986
Levi <jkv@levisen.eu>
2018-10-04 15:14:52 +0200

    fixed ref.

---
+++ Handin2/Code/AirTrafficMonitor.Unit.Test/TransponderDataParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NSubstitute;
+using TransponderReceiver;
+
+namespace AirTrafficMonitor.Unit.Test
+{
+    public class TransponderDataParser
+    {
+        private string _transponderData;
+        private string _newTransponderData;
+        private 
+    }
+}
