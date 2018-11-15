877a4f9214fecc55da12c1a595a37ef9090b061c
Levi <jkv@levisen.eu>
2018-11-16 00:34:13 +0100

    still failing on testing event triggered from other event. continuing

--- Handin3/Code/AirTrafficMonitor.Unit.Test/ITransponderDataConverterTest.cs
+++ Handin3/Code/AirTrafficMonitor.Unit.Test/ITransponderDataConverterTest.cs
@@ -14,52 +14,95 @@ namespace AirTrafficMonitor.Unit.Test
     [TestFixture]
     public class ITransponderDataConverterTest
     {
-        private DataConverter _uut;
-        private ITransponderReceiver _iTransponderReceiver;
+        private DataConverter _dataconverter;
+        private ITransponderReceiver _uut;
         private string _testData = "ATR423;39045;12932;14000;20151006213456789";
-        private readonly List<string> _listString;
-        private List<FTDataPoint> _listFTDataPoints;
         private int _eventsReceived;
-
+        private FTDataPoint _FTDataPoint;
 
         [SetUp]
         public void SetUp()
         {
-            //_iTransponderReceiver = Substitute.For<ITransponderReceiver>();
-            _iTransponderReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
-            _uut = new DataConverter(_iTransponderReceiver);
-            _listFTDataPoints = new List<FTDataPoint>();
+            _dataconverter = Substitute.For<DataConverter>();
+            _uut = TransponderReceiverFactory.CreateTransponderDataReceiver();
+            _FTDataPoint = _dataconverter.ConvertTransponderString(_testData);
 
-
-            var _listString = new List<string>();
-            _listString.Add(_testData);
-
-            _uut.FlightTrackDataReady += (o, args) => 
-            {
-                _listFTDataPoints = args.FTDataPoints;
-                _eventsReceived++;
-
-            };
 
         }
         [Test]
-        public void Transponderdata_EventTest_NoEvent()
+        public void Levi()
         {
             _eventsReceived = 0;
-            //var args = new RawTransponderDataEventArgs(_listString);
-            _iTransponderReceiver.TransponderDataReady += (sender, args) => _eventsReceived++;
-            Assert.That(_eventsReceived == 0);
+            _dataconverter.FlightTrackDataReady += (sender, args) => 
+            {
+                if (args.FTDataPoints.Count != 1) _eventsReceived++;
+            };
         }
+
+        //private DataConverter _uut;
+        //private ITransponderReceiver _iTransponderReceiver;
+        //private string _testData = "ATR423;39045;12932;14000;20151006213456789";
+        //private List<FTDataPoint> _listFTDataPoints;
+        //private int _eventsReceived;
+        //private List<string> _listString;
+
+        //public object A { get; private set; }
+
+        //[SetUp]
+        //public void SetUp()
+        //{
+        //    //_iTransponderReceiver = Substitute.For<ITransponderReceiver>();
+        //    _iTransponderReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+        //    _uut = new DataConverter(_iTransponderReceiver);
+        //    _listFTDataPoints = new List<FTDataPoint>();
+
+
+        //    _listString = new List<string>();
+        //    _listString.Add(_testData);
+
+        //    _uut.FlightTrackDataReady += (o, args) => 
+        //    {
+        //        _listFTDataPoints = args.FTDataPoints;
+        //        _eventsReceived++;
+
+        //    };
+
+        //}
         //[Test]
-        //public void Transponderdata_EventTest_OneEvent()
+        //public void Dataconverter_EventTest_NoEvent()
         //{
         //    _eventsReceived = 0;
         //    //var args = new RawTransponderDataEventArgs(_listString);
-        //    _iTransponderReceiver.TransponderDataReady += (sender, args) => 
-        //    {
-        //        if(args.TransponderData.)
-        //    };
+        //    _iTransponderReceiver.TransponderDataReady += (sender, args) => _eventsReceived++;
         //    Assert.That(_eventsReceived == 0);
         //}
+        //[Test]
+        //public void Dataconverter_EventTest_OneEvent()
+        //{
+        //    _eventsReceived = 0;
+        //    var args2 = new RawTransponderDataEventArgs(_listString);
+        //    _iTransponderReceiver.TransponderDataReady += (sender, args) => _eventsReceived++;
+        //    _uut.FlightTrackDataReady += (sender, args) => 
+        //    {
+        //        if (args.FTDataPoints.First(item => item.Tag != "daw") == "ATR423") _eventsReceived++;
+        //    };
+
+        //    _uut.Tra
+
+
+        //    Assert.That(_eventsReceived == 0);
+        //}
+        ////[Test]
+        ////public void Dataconverter_EventTest_OneEvent()
+        ////{
+        ////    _eventsReceived = 0;
+        ////    //var args = new RawTransponderDataEventArgs(_listString);
+        ////    _iTransponderReceiver.TransponderDataReady += (sender, args) =>
+        ////    {
+        ////        if (args.TransponderData.)
+        ////    };
+        ////    _uut.FlightTrackDataReady();
+        ////    Assert.That(_eventsReceived == 0);
+        ////}
     }
 }
